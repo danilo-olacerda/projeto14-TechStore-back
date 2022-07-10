f1ef78d3217b5409a2999bb0ec2366d115d88e64
Luis <lf.mc.almeida@poli.ufrj.br>
2022-07-10 17:46:29 -0300

    add description

--- src/middlewares/validateAdmin.js
+++ src/middlewares/validateAdmin.js
@@ -3,7 +3,7 @@ import newItemSchema from "../schemas/schemaAdmin.js";
 export default function validateAdmin(req, res, next){
     const validation = newItemSchema.validate(req.body);
     if (validation.error) {
-        return res.sendStatus(422);
+        return res.status(422).send(validation.error);
     }
 
     next();
